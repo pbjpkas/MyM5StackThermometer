a9a1c1f6e7a8df8251a0b5bada2ddb3e18055c5c
ka's <43340444+pbjpkas@users.noreply.github.com>
2020-01-02 17:02:54 +0900

    sampling the AUnit
    import AUnit basic.ino

--- MyM5StackThermometer.ino
+++ MyM5StackThermometer.ino
@@ -7,6 +7,8 @@
 #include <Wire.h>     //The DHT12 uses I2C comunication.
 DHT12 dht12;          //Preset scale CELSIUS and ID 0x5c.
 
+#line 11 "MyM5StackThermometer.ino"
+#include <AUnit.h>
 /*
 For configuration library:
 DHT12 dht12("Scale temperature","ID device for I2C");
@@ -22,6 +24,16 @@ the preset scale is KELVIN and ID is 0x5c.
   DHT12 dht12(FAHRENHEIT,0x53);
 The preset scale is FAHRENHEIT and ID is 0x53.
 */
+
+test(correct) {
+  int x = 1;
+  assertEqual(x, 1);
+}
+
+test(incorrect) {
+  int x = 1;
+  assertNotEqual(x, 1);
+}
 
 void setup() {
   M5.begin();
@@ -54,6 +66,8 @@ void loop() {
   Serial.print("Temperatura: ");
   Serial.print(dht12.readTemperature(KELVIN));
   Serial.println("*K");
+
+  while(1){ aunit::TestRunner::run();}
 
   delay(5000);
 }
